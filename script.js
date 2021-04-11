093ae26f220482862336fd2ae634ffbcf5405839
Luka <lukhanyo95@gmail.com>
2021-04-11 08:08:09 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const btn = document.getElementById('submit-btn');
+const reversedText = document.getElementById('reversed');
+const myForm = document.getElementById("my-form");
+
+
+
+const reverse = (str) =>{
+    let myString = '';
+
+    for(let i= str.length-1; i>=0 ;i--){
+        myString += str[i];
+    }
+    return myString;
+}
+
+myForm.addEventListener('submit', e =>{
+    e.preventDefault();
+    appendText();
+    myForm.reset();
+    })
+
+
+const appendText = () => {
+    const str = document.getElementById('word').value;
+    const text = document.createElement('h3');
+    text.textContent = reverse(str);
+    reversedText.appendChild(text)
+    
+    }
